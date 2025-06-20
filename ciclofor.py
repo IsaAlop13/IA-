33de267bba1a0c9a704085c91f7aea04dd4f8f0b
Usuario Sala Calima <usr-calima@ugca.local>
2025-06-20 08:44:54 -0500

    Segundo commit

--- ciclofor.py
+++ ciclofor.py
@@ -1,4 +1,12 @@
+"""
 tabla = int(input("Cuál tabla quiere?: "))
 for i in range (1,11):
     print(tabla, "x", i, "=", tabla*i)
-    
+"""    
+print("Ingrese dos valores enteros para las operaciones")
+a = int(input("Primer número: "))
+b = int(input("Segundo número: "))
+print("Resultado suma=",a+b)
+print("Resultado resta=",a-b)
+print("Resultado multiplicación=",a*b)
+print("Resultado división=",a//b)
